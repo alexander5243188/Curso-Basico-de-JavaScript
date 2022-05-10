1e41dbbd2ebf640ec0bbde9ec0ab7e86d765c445
ale <alexander5243188@yahoo.es>
2022-05-10 07:58:47 -0400

    Alejandra amor, ya tenemos declarado los tipos de valores

---
+++ valores.js
@@ -0,0 +1,20 @@
+//Estos son valores primitivos
+26 //numero
+
+"Graciela Alejandra" //string
+
+true //boolean
+false //boolean
+
+null //objeto
+undefined //objeto
+
+
+//valores objetos o no priovitivos
+[1,2,3,4,5] //array tipo objeto
+
+{nombre:"Graciela", edad; 26} //objeto
+
+
+//Operadores
+typeof nombre //devuelve el tipo de dato
