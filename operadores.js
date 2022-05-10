65b2a94c385b9336d6f56b763d1d48ed23b84f33
ale <alexander5243188@yahoo.es>
2022-05-10 19:59:09 -0400

    Alejandra mi amor, estos son operadores binarios

---
+++ operadores.js
@@ -0,0 +1,8 @@
+//Operadores binarios
+let alejandra = 26;
+let alexander = 42;
+
+let suma = alejandra + alexander;
+let resta = alejandra - alexander;
+let multiplicacion = alejandra * alexander;
+let division = alejandra / alejandra;
