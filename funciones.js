b886c775855e19eedd679b95ee097132ce800d4f
ale <alexander5243188@yahoo.es>
2022-05-10 17:22:37 -0400

    Alejandra mi amor trabajamos con funciones

---
+++ funciones.js
@@ -0,0 +1,11 @@
+//Funcion declarativa
+function miAlejandra(){
+    return "Hola, soy Alejandra";
+}
+miAlejandra()
+//Funcion expresiva o funcion anonima
+var miAlejandra = function(pensamiento){
+    return `Hola, soy Alejandra ${pensamiento}`
+}
+
+miAlejandra(" mi amor");
---
+++ variables.js
@@ -0,0 +1,19 @@
+var  nombre = "Graciela mi amor";
+console.log(nombre);
+
+//declarar una variable
+var edad;
+//inicializar una variable
+edad = 26;
+console.log(edad);
+
+//variables mas complejas
+var nombre = ["Graciela", "Alejandra", "mi amor"];
+
+//variables con tipos de objetos
+var persona={
+    nombre: "Graciela Alejandra",
+    apellido: "mi amor",
+    edad: 26
+}
+persona;
