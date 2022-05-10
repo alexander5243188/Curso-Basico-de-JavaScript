5cd0634c7f8c058cfe5bafd52e3e4d7a9e2e50d6
ale <alexander5243188@yahoo.es>
2022-05-10 19:08:41 -0400

    Alejandra mia amor, trabajamos con hoisting

---
+++ hoisting.js
@@ -0,0 +1,13 @@
+var tuNombreMiAmor;
+var tuNombreMiAmor = "Alejandra";
+
+
+console.log(tuSegundoNombreMiAmor);
+var tuSegundoNombreMiAmor = "Graciela";
+
+tuApellidoMiAmor();
+
+function tuApellidoMiAmor() {
+    return "Albarracin"+tuNombreMiAmor;
+}
+var tuNombreMiAmor = "Alejandra";
