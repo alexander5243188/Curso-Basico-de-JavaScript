d2b4c18fdbebbb7f191e767b97d6e4431d2ed975
ale <alexander5243188@yahoo.es>
2022-05-11 09:47:13 -0400

    Alejandra mi amor, trabajamos con if else y el operador ternario

---
+++ ifelse.js
@@ -0,0 +1,20 @@
+//condicional if
+
+if(false){
+    console.log("Hola mi amor Alejandra")
+}
+else{
+    console.log("Te quiero Alejandra");
+}
+let alejandra = 26;
+let alexander = 42;
+
+if(alejandra < alexander){
+    console.log("Hola mi amor te quiero mucho Alejandra");
+}
+else if(alejandra > alexander){
+    console.log("Hola mi amor te quiero mucho Alexander");
+}
+
+let sentimiento = alejandra < alexander ? "Alexander eres mayor pero tambien el amor es mucho mayor":"Alejandra eres mayor pero tambien el amor es mucho mayor";
+console.log(sentimiento);
