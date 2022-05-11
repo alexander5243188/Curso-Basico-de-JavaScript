fadc24f04028899dc149b354922333dcf6b2c321
ale <alexander5243188@yahoo.es>
2022-05-10 20:33:13 -0400

    Alejandra mi amor, trabajamos con operadores

--- operadores.js
+++ operadores.js
@@ -6,3 +6,48 @@ let suma = alejandra + alexander;
 let resta = alejandra - alexander;
 let multiplicacion = alejandra * alexander;
 let division = alejandra / alejandra;
+
+//concatenacion
+let segundoNombreAlejandra = "Graciela";
+let segundoNombreAlexander = "Richard";
+let mensaje = "Hola " + segundoNombreAlejandra + " y " + segundoNombreAlexander;
+console.log(mensaje);
+
+//negacion
+let sentimientoAlejandra = true;
+console.log(!sentimientoAlejandra);
+
+//Asignacion
+let teQuieroAlejandra = "Si";
+console.log(teQuieroAlejandra);
+
+//Comparacion
+let comparacion = 26 ==  "26";
+console.log(comparacion);
+
+let otraComparacion = 26 === "26";
+console.log(otraComparacion);
+
+//Mayor que
+let esMayor = alejandra > alexander;
+
+//Menor que
+let esMenor = alejandra < alexander;
+
+//Mayor o igual que
+let esMayorOIgual = alejandra >= alexander;
+
+//Menor o igual que
+let esMenorOIgual = alejandra <= alexander;
+
+//Operador and
+let and = alejandra > alexander && alexander > alejandra;
+
+//Operador or
+let or = alejandra > alexander || alexander > alejandra;
+
+//Incrementar por uno
+let incremento = alejandra++;
+
+//Incrementar por dos
+let incrementoDos = alejandra += 2;
